ea38aea783dfeee06b0da23572cd70200879fc02
henrique-britoleao <hbl451@gmail.com>
2023-10-18 19:04:50 +0200

    Fix typo

--- src/modelling/main.py
+++ src/modelling/main.py
@@ -16,14 +16,14 @@ app = typer.Typer()
 def main(trainset_path: Path) -> None:
     """Train a model using the data at the given path."""
     logger.info(f"Training model using data at {trainset_path}")
-    logger.info(f"Processing data...")
+    logger.info("Processing data...")
     data = pd.read_csv(trainset_path)
     data.columns = [col.replace(" ", "_").lower() for col in data.columns]
     X = data.drop(TARGET, axis=1)
     y = data[TARGET]
     X, encoder = preprocess(X)
     pickle_object(encoder, OBJECTS_PATH / "encoder.pkl")
-    logger.info(f"Training model...")
+    logger.info("Training model...")
     model = train_model(X, y)
     pickle_object(model, OBJECTS_PATH / "model.pkl")
 
