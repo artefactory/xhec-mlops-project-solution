656304367b8f22d4a9b9682a2a2b4df90bb15304
Luca Serra <luca.serra@hotmail.fr>
2023-10-19 11:07:34 +0200

    enh: add values constraint on abalone sex

--- src/web_service/lib/models.py
+++ src/web_service/lib/models.py
@@ -1,10 +1,17 @@
+from enum import Enum
 from typing import List
 
 from pydantic import BaseModel
 
 
+class AbaloneSex(str, Enum):
+    male = "M"
+    female = "F"
+    infant = "I"
+
+
 class InputAbaloneData(BaseModel):
-    sex: str
+    sex: AbaloneSex
     length: float
     diameter: float
     height: float
@@ -13,6 +20,11 @@ class InputAbaloneData(BaseModel):
     viscera_weight: float
     shell_weight: float
 
+    class Config:
+        use_enum_values = True
+
 
 class PredictionOut(BaseModel):
+    predicted_abalone_ages: List[float]
+    predicted_abalone_ages: List[float]
     predicted_abalone_ages: List[float]
