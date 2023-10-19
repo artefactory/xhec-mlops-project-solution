949187a08c686197d5ec78631e599687af6c87a8
Henrique <71887453+henrique-britoleao@users.noreply.github.com>
2023-10-19 09:46:10 +0200

    Update requirements (#1)
    * Update requirements
    
    * Fix typo

--- src/modelling/main.py
+++ src/modelling/main.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import pandas as pd
 import typer
+from loguru import logger
 
 from config.config import OBJECTS_PATH, TARGET
 from src.modelling.preprocessing import preprocess
@@ -14,12 +15,15 @@ app = typer.Typer()
 @app.command()
 def main(trainset_path: Path) -> None:
     """Train a model using the data at the given path."""
+    logger.info(f"Training model using data at {trainset_path}")
+    logger.info("Processing data...")
     data = pd.read_csv(trainset_path)
     data.columns = [col.replace(" ", "_").lower() for col in data.columns]
     X = data.drop(TARGET, axis=1)
     y = data[TARGET]
     X, encoder = preprocess(X)
     pickle_object(encoder, OBJECTS_PATH / "encoder.pkl")
+    logger.info("Training model...")
     model = train_model(X, y)
     pickle_object(model, OBJECTS_PATH / "model.pkl")
 
