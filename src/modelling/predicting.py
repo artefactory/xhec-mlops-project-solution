f8b81a7c372750b35b17af538a5aa4195b626382
Luca Serra <luca.serra@hotmail.fr>
2023-10-19 11:46:01 +0200

    feat: add prefect for training flow

--- src/modelling/predicting.py
+++ src/modelling/predicting.py
@@ -1,7 +1,9 @@
 from numpy.typing import ArrayLike
+from prefect import task
 from sklearn.base import BaseEstimator
 
 
+@task(name="Prediction")
 def predict(X: ArrayLike, model: BaseEstimator) -> ArrayLike:
     """Predict the given data using the given model."""
     return model.predict(X)
